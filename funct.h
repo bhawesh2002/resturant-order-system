90855be970b5f642f26e97ee1480756be822c042
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-02-21 08:19:24 +0530

    create a menu function

---
+++ funct.h
@@ -0,0 +1,42 @@
+#ifndef FUNCT_H
+#define FUNCT_H
+#include <iostream>
+#include <string>
+
+void menu(){
+        std::cout << "====Breakfast===="
+              << "\n1.Idli"
+              << "\t2.Plain Dosa"
+              << "\t3.Masala Dosa"
+              << "\t4.Uttapam"
+              << "\n5.Samosa"
+              << "\t6.BreadPakoda"
+              << "\t7.Pani Puri"
+              << "\t8.Pav Bhaji"
+              << "\n9.Chilli Paneer"
+              << "\t10.Chola Puri"
+              << "\t11.Sambar Vada"
+              << "\t12.Rava"
+              << "\n13.Bread"
+              << "\t14.Pizza"
+              << "\t15.Burger"
+              << "\t16.Sandwich"
+              << "\n17.Pasta"
+              << "\t18.Noodles"
+              << "\t19.Pulav"
+              << "\t20.Soup"
+              << "\n21.Pastery"
+              << "\t22.Cake"
+              << "\t23.Ice Cream"
+              << "\t24.Coffee"
+              << "\n25.Tea"
+              << "\t26.Juice"
+              << "\t27.Milk"
+              << "\t28.Curd"
+              << "\n29.Butter"
+              << "\t30.Cheese"
+              << std::endl;
+
+}
+
+#endif
