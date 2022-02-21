879871b92e437d4c969872741bf6839c42f223d5
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-02-21 08:19:54 +0530

    call to menu funnction

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include "funct.h"
 
-int main(){
-    std::cout << "WELCOME TO RAJBHOG" << std::endl;
-    std::cout << "====ORDER MENU====" << std::endl;
+int main()
+{
+    std::cout << "WELCOME TO RAJBHOG" << std::endl; // Welcome Note
+    std::cout << "====ORDER MENU====" << std::endl; // Menu
+    menu(); // Calling menu function
     return 0;
 }
