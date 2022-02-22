7ea353fa11c19a7caaf15b3e9cf27d2768e197b6
bhawesh2002 <bhaweshmankar2002@gmail.com>
2022-02-23 03:31:13 +0530

    call to write_menu function

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ int main()
 {
     std::cout << "WELCOME TO RAJBHOG" << std::endl; // Welcome Note
     std::cout << "====ORDER MENU====" << std::endl; // Menu
+    write_menu();
     show_menu(); // Calling menu function
     get_choice();
     std::cout << "We are cooking for you"
