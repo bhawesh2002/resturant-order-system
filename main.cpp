48bf508b9ae7fa0698b8616b5b2d476e4aea7a98
bhawesh2002 <bhaweshmankar2002@gmail.com>
2022-02-23 09:40:44 +0530

    add price field  from write_menu

--- main.cpp
+++ main.cpp
@@ -5,9 +5,10 @@ int main()
 {
     std::cout << "WELCOME TO RAJBHOG" << std::endl; // Welcome Note
     std::cout << "====ORDER MENU====" << std::endl; // Menu
-    write_menu();
-    show_menu(); // Calling menu function
-    get_choice();
+    write_menu(); // Write Menu
+    std::cout << "ITEM'S" << "\t\tPRICE";
+    show_menu(); // calling show_menu function
+    get_choice(); //get the order from the costumer
     std::cout << "We are cooking for you"
               <<  "\nYour order will be ready in 15 minutes.\n";
     return 0;
